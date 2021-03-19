fb82e134d32f114b0afdf04f821b7c6d80b677ff
unknown <dibatalibjali@gmail.com>
2021-03-19 12:25:44 +0800

    updates

--- my-covid-mining/src/main/java/com/app/mining/controller/MyCovidMiningController.java
+++ my-covid-mining/src/main/java/com/app/mining/controller/MyCovidMiningController.java
@@ -1,13 +1,10 @@
 package com.app.mining.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.mining.service.covid.api.CovidMiningAPITotalCases;
-import com.app.model.CovidCasesArea;
 
 import lombok.extern.slf4j.Slf4j;
 
